945b583b036c24a0f71aaa302e827f89a416efff
avalos <ivan.avalos.diaz@hotmail.com>
2019-07-05 15:08:22 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Font viewer
+
+Font viewer is a tool that allows you to visualise easily your favourite fonts, allowing you to choose between Google fonts and system fonts, with the posibility to load custom ones from CDNs. You can also choose random fonts and many other interesting stuff.
+
+## Configure
+
+In order to be able to load fonts from Google Fonts, you need to create an API key from [here](https://developers.google.com/fonts/docs/developer_api) and set it into the `key` variable in `another.js` file.
+
+```js
+// +--------------------------------------+
+// |  SET YOUR GOOGLE API KEY RIGHT HERE  |
+// +--------------------------------------+
+var key = 'AIza...';
+```
+
+## Credits
+
+This website is powered by the amazing [Bootstrap](https://getbootstrap.com) CSS framework and the powerful [jQuery](https://jquery.org) library for DOM manipulation, both licensed under the MIT licence.
+
+## Licence
+
+This project is licensed under the GNU General Public Licence v3.
+
+	Font Viewer - visualise your favourite fonts easily.
+    Copyright (C) 2019  Iván Ávalos <ivan.avalos.diaz@hotmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
---
+++ js/another.js
@@ -0,0 +1,139 @@
+/*
+ *  Font Viewer - visualise your favourite fonts easily.
+ *  Copyright (C) 2019  Iván Ávalos <ivan.avalos.diaz@hotmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// +--------------------------------------+
+// |  SET YOUR GOOGLE API KEY RIGHT HERE  |
+// +--------------------------------------+
+var key = '<GOOGLE_FONTS_API_KEY>';
+
+var xhr = new XMLHttpRequest();
+var fonts = [];
+var fontcache = [];
+
+$(document).ready(function () {
+	// Load font list from Google Fonts
+	xhr.open('GET', 'https://www.googleapis.com/webfonts/v1/webfonts?key=' + key, true);
+	xhr.send();
+	xhr.addEventListener("readystatechange", loadfonts, false);
+
+	$('#font-selector').on('change', function () {
+		loadfont(this.value);
+	});
+});
+function loadfonts (e) {
+	if (xhr.readyState == 4 && xhr.status == 200) {
+		var json = JSON.parse(xhr.responseText);
+		var prev = '';
+		var curr = '';
+		for (var i = 0; i < json.items.length; i++){
+			const typeface = json.items[i].family;
+			fonts.push(typeface);
+			curr = typeface[0].toUpperCase();
+			var optgroup = null;
+			if (prev != curr) {
+				optgroup = $('<optgroup/>', { label: curr, id: 'group-' + curr });
+				$('#font-selector').append(optgroup);
+			} else {
+				optgroup = $('#group-' + prev);
+			}
+			const option = $('<option/>', { value: typeface, html: typeface });
+			optgroup.append(option);
+			prev = curr;
+		}
+	}
+
+	// Load CDNs from cookies
+	const cdns = JSON.parse(localStorage['cdns'] || '[]');
+	for (var i = 0; i < cdns.length; i++) {
+		let cdn = $(cdns[i]);
+		$('head').append(cdn);
+	}
+
+	// Load fonts from cookies
+	const type = localStorage['type'] || 'google';
+	if (type == 'system') {
+		$('#custom-font').val(localStorage['font']);
+		loadsys ('#custom-font');
+	}
+	if (type == 'google') {
+		$('#font-selector').val(localStorage['font'] || 'Montserrat');
+		loadfont(localStorage['font'] || 'Montserrat');
+	}
+}
+
+function randomfont () {
+	const font = fonts[Math.floor(Math.random()*fonts.length)];
+	$('#font-selector').val(font);
+	loadfont(font);
+}
+
+function insertcdn () {
+	const cdn = $('#custom-cdn').val();
+	const link = $(cdn);
+	$('head').append(link);
+	$('#custom-cdn').val('');
+	storecdn(cdn);
+}
+
+function loadsys (selector) {
+	const font = $(selector).val();
+	$('body').css ('font-family', font);
+	localStorage['type'] = 'system';
+	localStorage['font'] = font;
+}
+
+function loadfont (font) {
+	$('body').css('font-family', font);
+	localStorage['type'] = 'google';
+	localStorage['font'] = font;
+	if (fontcache.includes(font)) {
+		return;
+	}
+	fontcache.push(font);
+	const css = $('<link/>', {
+		href: fonturl(font),
+		rel: 'stylesheet'
+	});
+	$('head').append(css);
+	storecdn (css.prop('outerHTML'));
+}
+
+function fonturl (font) {
+	var url = 'https://fonts.googleapis.com/css?family=';
+	url += font.replace(' ', '+');
+	url += ':100,200,300,400,500,600,700,800,900';
+	url += '&display=swap';
+	return url;
+}
+
+function getcdns () {
+	return JSON.parse (localStorage['cdns'] || '[]');
+}
+
+function storecdn (cdn) {
+	const cdns = JSON.parse(localStorage['cdns'] || '[]');
+	if (cdns.includes(cdn)) {
+		return;
+	}
+	cdns.push(cdn);
+	localStorage['cdns'] = JSON.stringify(cdns);
+}
+
+function clearstorage () {
+	localStorage.clear();
+}
